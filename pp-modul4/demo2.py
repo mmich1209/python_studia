9d371904f2c4c5668be7a7067f661ca04e6812b9
mmich1209 <mmichalski1209@gmail.com>
2024-11-08 10:23:31 +0100

    Laby na zajecia, done

---
+++ pp-modul4/demo2.py
@@ -0,0 +1,16 @@
+# Zrobimy tutaj zadania z laba, ktore juz mam zrobione, ale tak zeby sobie przypomniec
+
+# print("To jest testowy napis.")
+# print(type("To jest testowy napis."))
+# print(len("To jest testowy napis."))
+
+# print(len(xxx) # len dziala tylko z str
+# print(len(0b1101))
+# TypeError: object of type 'int' has no len()
+
+print(bool(1))
+print(bool(-15))
+print(bool(0))
+
+print
+print(1*15**1 + 6 * 15*)
